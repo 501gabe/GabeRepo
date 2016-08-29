02a8b57a8a4890acd1c0a31ea5115addcb6b0bd3
Gabriel Martindale <martindale.gabriel@gmail.com>
2016-08-28 19:39:56 -0500

    Android app ideas for final project

---
+++ TemperatureCOneversion/app/src/main/java/com/gabe/android/aca/temperatureconeversion/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gabe.android.aca.temperatureconeversion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        float fah = 86; //fahrenheit
+        System.out.println(fah + "degrees in Fahrenheit is....");
+
+
+
+
+    }
+}
+
