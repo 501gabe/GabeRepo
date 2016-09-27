ba8ec5b030df802bd7435ffd9dee21030b66444e
Gabriel Martindale <Martindale.gabriel@gmail.com>
2016-09-27 13:30:21 -0500

    Initial Commit: Where It's Snap app

---
+++ WhereItsSnap/app/src/main/java/com/gabe/android/aca/whereitssnap/TitlesFragment.java
@@ -0,0 +1,18 @@
+package com.gabe.android.aca.whereitssnap;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+/**
+ * Created by Gabe on 9/26/16.
+ */
+
+public class TitlesFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
